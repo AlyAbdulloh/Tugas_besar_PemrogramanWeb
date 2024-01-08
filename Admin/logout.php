193905a9b586f2e88025a3444ad8588f0ae5223f
AlyAbdulloh <uwongsabar12@gmail.com>
2024-01-08 14:15:25 +0700

    fix: rename directory

--- Admin/logout.php
+++ Admin/logout.php
@@ -1,6 +1,5 @@
 <?php
-    session_start();
-    session_destroy();
+session_start();
+session_destroy();
 
-    header('location:../RegisLogin/RegAndLog.php');
-?>
+header('location:../Auth/RegAndLog.php');
